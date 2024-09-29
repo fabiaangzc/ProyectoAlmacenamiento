ce8af58ff905eb0e5e61fb0179ed6d2db5045fe9
Fabián Gomezcasseres <161890830+fabiaangzc@users.noreply.github.com>
2024-09-28 21:11:21 -0500

    Creé tabla catedoría nuevamente

--- backend/src/main/java/com/apiweb/backend/Controller/CategoriaController.java
+++ backend/src/main/java/com/apiweb/backend/Controller/CategoriaController.java
@@ -3,38 +3,24 @@ package com.apiweb.backend.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.apiweb.backend.Model.CategoriaModel;
-import com.apiweb.backend.Service.ICategoriaService;
+import com.apiweb.backend.Service.CategoriaService;
 
 @RestController
-@RequestMapping("/apiweb/v1/categorias")
+@RequestMapping ("/apiweb/v1/categorias")
 public class CategoriaController {
 
     @Autowired
-    ICategoriaService categoriaService;
+    CategoriaService categoriaService;
 
     @PostMapping("/insertar")
     public ResponseEntity<String> crearCategoria(@RequestBody CategoriaModel categoria) {
-        categoriaService.guardarCategoria(categoria);
-        return new ResponseEntity<>("Categoría creada exitosamente", HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<CategoriaModel> obtenerCategoriaPorId(@PathVariable int id) {
-        CategoriaModel categoria = categoriaService.obtenerCategoriaPorId(id);
-        if (categoria != null) {
-            return new ResponseEntity<>(categoria, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/eliminar/{id}")
-    public ResponseEntity<String> eliminarCategoria(@PathVariable int id) {
-        categoriaService.eliminarCategoria(id);
-        return new ResponseEntity<>("Categoría eliminada exitosamente", HttpStatus.OK);
+        return new ResponseEntity<String>(categoriaService.guardarCategoria(categoria), HttpStatus.OK);
     }
 }
 
--- backend/src/main/java/com/apiweb/backend/Repository/ICategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apiweb.backend.Repository;
-
-import com.apiweb.backend.Model.CategoriaModel;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoriaRepository extends JpaRepository<CategoriaModel, Integer> {
-    // futuras consultas 
-}
