c3f960ea41edb881ff476ad5bbe1293e3089c9e6
JuanHoyos329 <juanandres8000@hotmail.com>
2024-10-06 10:08:13 -0500

    Se le quito a valoracion la fecha, ya que esta la pone sola la base de datos.

--- backend/src/main/java/com/apiweb/backend/Model/ValoracionModel.java
+++ backend/src/main/java/com/apiweb/backend/Model/ValoracionModel.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -22,8 +21,7 @@ public class ValoracionModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idValoracion;
-    private LocalDate fecha;  
+    private Integer idValoracion;  
 
     @ManyToOne
     @JoinColumn(name = "idUsuario")
